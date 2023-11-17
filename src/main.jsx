be6c085f23772c10f0df4d63370ba2f63c515d77
UirleandroSantos <uirleandro.santos.19@gmail.com>
2023-11-17 09:21:47 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import { App } from './App'
 
 import { Route, Routes, BrowserRouter as Router, BrowserRouter } from 'react-router-dom';
 import { Contacts } from './components/pages/contacts/Contacts'
-import { Projects } from './components/pages/projects/Projects'
+// import { Projects } from './components/pages/projects/Projects'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
+      <Route path='/' element={<App />}/>
       <Route path="/portifolio" element={<App />} />
       <Route path="/contacts" element={<Contacts />} />
     </Routes>
