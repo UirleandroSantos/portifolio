e7943dcbe70d2428a02ca3bec2b999e86c08aed7
UirleandroSantos <uirleandro.santos.19@gmail.com>
2023-11-17 09:50:07 -0300

    add Footer

--- src/components/pages/contacts/Contacts.jsx
+++ src/components/pages/contacts/Contacts.jsx
@@ -4,8 +4,6 @@ import styles from './contacts.module.css';
 import iconFacebook from '../../../assets/facebook.svg';
 import iconWhatsapp from '../../../assets/whatsapp.svg';
 import iconIntagram from '../../../assets/instagram.svg';
-import iconDiscord from '../../../assets/discord.svg';
-import iconGit from '../../../assets/GitHub.svg';
 
 export function Contacts(){
     return (
@@ -28,6 +26,7 @@ export function Contacts(){
                         <p>Instagram</p>
                     </a>
                 </div>
+                <Footer />
             </section>
         </section>
 )
