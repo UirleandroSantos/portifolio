41725f7897c59f48aeccd2be1cdade16ab8762bc
UirleandroSantos <uirleandro.santos.19@gmail.com>
2023-11-25 01:09:10 -0300

    update

---
+++ src/components/pages/error/Error.jsx
@@ -0,0 +1,10 @@
+import { Header } from "../../header/Header";
+
+export function Error(){
+    return (
+        <>
+            <Header />
+            <h1>Página não encontrada!</h1>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { App } from './App'
 
 import { Route, Routes, BrowserRouter as Router, BrowserRouter } from 'react-router-dom';
 import { Contacts } from './components/pages/contacts/Contacts'
+import { Error } from './components/pages/error/Error';
 // import { Projects } from './components/pages/projects/Projects'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='/portifolio'>
     <Routes>
+      <Route path='*' element={<Error />} />
       <Route path='/' element={<App />}/>
       <Route path="/portifolio" element={<App />} />
       <Route path="/contacts/" element={<Contacts />} />
